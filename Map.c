2945ad3d51ac72274daa213a54b5e7ff8d4cee1f
A.F <adrian6@hotmail.fr>
2022-10-19 00:58:56 +0200

    Can now display the map, the players and the bombs

--- Map.c
+++ Map.c
@@ -61,3 +61,46 @@ void freeMap(Map *m, int r){
     freeTab(m->tab, r);
     free(m);
 }
+
+
+/* Creation de plateaux de jeu
+// premiere map test : 7x7 :
+// 0 : vide                     (255)
+// 1 : mur destructible         (176)
+// 2 : mur infranchissable      (219)
+// 3 : J1   (80)
+// 4 : J2   (80)
+// 5 : J3   (80)
+// 6 : J4   (80)
+// 7 : bombe J1     (ò)
+// 8 : bombe J2
+// 9 : bombe J3
+// 10 : bombe J4
+*/
+void displayMap(int r, int c, int map[r][c]){
+    int i, j;
+    int graphismesHD[] = {255, 176, 219, 80, 162};
+
+    for(i = 0; i < r; ++i){
+        for (j = 0; j < c; ++j){
+            int sprite = map[i][j];
+
+            if (i == 0 ||i == r-1){
+                if(j != c-1){
+                    printf("%c", 219);
+                }
+            } else if(j > 0){
+                printf(" ");
+            }
+
+            if(sprite > 2 && sprite < 7){
+                printf("%c", graphismesHD[3]);
+            } else if (sprite > 6 ){
+                printf("%c", graphismesHD[4]);
+            } else {
+                printf("%c", graphismesHD[sprite]);
+            }
+        }
+        printf("\n");
+    }
+}
