b5ae19100b7234aab682dd666139c5f9c4e96f38
A.F <adrian6@hotmail.fr>
2022-10-23 23:50:07 +0200

    Add more TODO in main

--- main.c
+++ main.c
@@ -1,16 +1,14 @@
 #include "Map.h"
 
 int main(int argc, char** argv) {
-    int rows = 2;
+    /*int rows = 2;
     int columns = 3;
     Map *m = createMap(rows,columns);
 
     printf("%d ", isFree(m, 0, 0));
     freeMap(m, rows);
 
-
-    printf("%c\n", 176);
-
+    ///////////////////////////////////////////////////////
     // MAP D'EXEMPLE
     int map1[][7] = {
             {2,2,2,2,2,2,2},
@@ -22,17 +20,53 @@ int main(int argc, char** argv) {
             {2,2,2,2,2,2,2}
     };
     displayMap(7, 7, map1);
+    */
+
+    // Nombre de joueurs à placer
+    int nmbPlayer;
+    printf("Nombre de joueurs (max : 4) :");
+    scanf("&d", nmbPlayer);
+    fflush(stdin);
+
+    if(nmbPlayer < 2 || nmbPlayer > 4){
+        nmbPlayer = 2;
+    }
+
+    // Nombre de bots parmi ces joueurs
+    int nmbBots;
+    printf("Nombre de bots (max : %d, defaut : 0) :", nmbPlayer);
+    scanf("&d", nmbBots);
+    fflush(stdin);
+
+    if(nmbBots < 0 || nmbBots > nmbPlayer){
+        nmbBots = 0;
+    }
+
+
+    // TODO : sélectionner une map dans la pool et récupérer les informations de cette map dans un tableau
+
+
+
+
+    // TODO : créer les joueurs en fonction des paramètres de la map
+
+    
 
 
     // === BOUCLE DE JEU ===
+    int actualPlayer = 1;
     int end = 0;
     char input;
 
     while(end != 1){
         // TODO : Fonction Affichage ATH
+        // Faut aller chercher dans la structure des joueurs leurs paramètres actuels.
+        // rajouter en paramètre de la fonction le numéro du joueur pour pouvoir afficher genre "Tour du joueur 1"
+        // rajouter en paramètre de la fonction un int code erreur pour afficher genre "Coup invalide. Rejouer"
 
         // Affichage map
         displayMap(7, 7, map1);
+
 
         // Input
         printf("Action a effectuer :");
@@ -43,7 +77,7 @@ int main(int argc, char** argv) {
         //TODO : Fonction Vérification d'input
         int verifInput = fonctionVerifInputQuiEstACoder();
 
-        // Si input pas OK
+        // Si input pas OK on revient au début de la boucle
         if(verifInput == 0) {
             continue;
         }
@@ -51,15 +85,24 @@ int main(int argc, char** argv) {
         // TODO : Màj de la map
 
 
+
         // TODO : Tik des bombes
+
 
 
         // TODO : explosion des bombes (peut être mettre ça dans la fonction de tik des bombes)
 
 
-        // TODO : Màj de la map
-        
 
+        // TODO : Màj de la map
+
+
+
+        // TODO : changement du joueur
+        if(actualPlayer == nmbPlayer) {
+            actualPlayer = 1;
+        } else {
+            ++actualPlayer;
         }
 
     }
